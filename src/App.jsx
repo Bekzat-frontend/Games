25a9e23dc9ef9eb97564077848a2abc8a3c2d8c0
Bekzat-frontend <mamateminovbekzat793@gmail.com>
2025-01-21 00:17:39 +0700

    ggggg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Board from "./Board";
-import './App.css'
+import "./App.css";
+import { Button } from "./Button";
 
 function App() {
-    return (
-        <div className="App">
-            <Board/>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Board />
+      <Button />
+    </div>
+  );
 }
 
 export default App;
